514ac132a4af43642bebec949e6590072ef14c00
Sushavan Karmakar <sushavan.karmakar@gmail.com>
2025-09-04 20:59:00 +0530

    test

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import random
 import string
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
